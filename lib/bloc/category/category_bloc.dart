5c21fa0414c59f34712a41fd6b8685fb3de99349
saeedboghraeii <boghraeii@gmail.com>
2025-08-21 08:38:52 +0330

    fix bog error project

--- lib/bloc/category/category_bloc.dart
+++ lib/bloc/category/category_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:online_shop/bloc/category/category_event.dart';
 import 'package:online_shop/bloc/category/category_state.dart';
 import 'package:online_shop/data/di/di.dart';
+
 import 'package:online_shop/data/repository/category_repository.dart';
 
 class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
